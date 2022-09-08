0ad13781ccb23747b767425b926a6f5e6b9f7cbe
Farvez Abdullah <engrfrzabdullah999@gmail.com>
2022-09-08 22:18:10 +0600

    Primitive and non-primitive data type

---
+++ primitive-non-primitive-data-type.js
@@ -0,0 +1,12 @@
+// JavaScript is dynamic typed programming Launguge
+// Primitive data type
+const a = 5;
+const b = 'Farvez'
+const c = true;
+
+
+// Non-primitive data type
+const d = [10, 20, 30, 40, 50]
+const e = { name: 'Farvez', age: 24, education: 'BSc in CSE' }
+
+console.log(typeof (a), typeof (b), typeof (c), typeof (d), typeof (e))
