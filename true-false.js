2e58612cb95437d3508a7e33153e1586fcddc464
Farvez Abdullah <engrfrzabdullah999@gmail.com>
2022-09-09 09:06:00 +0600

    Truthy and Falsy values in JavaScript

---
+++ true-false.js
@@ -0,0 +1,36 @@
+/*
+   Truthy:
+   1. true
+   2. any number (+ve,-ve) will be trurhy othen then 0
+   2. any string will be trurhy othen then empty string 
+   3. '0'
+   4. 'false'
+   5. {}
+   6. []
+
+   Falsy:
+   1. false
+   2. 0
+   3. '' empty string
+   4. undefind
+   5. null
+*/
+const x = 0;
+if (x) {
+    console.log('X is Truthy');
+}
+else {
+    console.log('x is Falsy')
+}
+
+// optational 
+// check falsy 
+const p = '';
+if (!p) {
+    console.log('p is palsy')
+}
+// check truthy 
+const q = ' ';
+if (!!q) {
+    console.log('q is truthy')
+}
